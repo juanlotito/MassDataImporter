74517a9cab2b4e482bf7d080ecefda8d6e290134
Juan Ignacio Lotito <jlotito@nosis.com>
2024-05-07 16:02:58 -0300

    feature: add txt service method

--- Services/ImportacionService.cs
+++ Services/ImportacionService.cs
@@ -1,8 +1,8 @@
-﻿using importacionmasiva.api.net.DataBase;
-using importacionmasiva.api.net.Repositories.Interface;
+﻿using importacionmasiva.api.net.Repositories.Interface;
 using importacionmasiva.api.net.Services.Interface;
 using importacionmasiva.api.net.Utils.Excel;
 using importacionmasiva.api.net.Utils.Exceptions;
+using importacionmasiva.api.net.Utils.Txt;
 using System.Data;
 
 namespace importacionmasiva.api.net.Services
@@ -11,6 +11,7 @@ namespace importacionmasiva.api.net.Services
     {
         private readonly IImportacionRepositories _importacionRepositories;
         private readonly ExcelUtils _excelUtils = new ExcelUtils();
+        private readonly TxtUtils _txtUtils = new TxtUtils();
 
         public ImportacionService(IImportacionRepositories importacionRepositories)
         {
@@ -49,5 +50,39 @@ namespace importacionmasiva.api.net.Services
                 throw new CustomException(500, ex.Message);
             }
         }
+
+        public async Task ImportFromTxt(IFormFile dataset, string registryName, string tableName)
+        {
+            try
+            {
+                DataTable dataTable = _txtUtils.ReadTxtToDataTable(dataset);
+
+                if (await _importacionRepositories.TableExists(tableName, registryName))
+                {
+                    var tableDefinition = await _importacionRepositories.GetTableDefinition(tableName, registryName);
+
+                    if (tableDefinition.Columns.Count != dataTable.Columns.Count)
+                        throw new CustomException(400, "El número de columnas no coincide con el de la tabla existente.");
+
+                    for (int i = 0; i < tableDefinition.Columns.Count; i++)
+                    {
+                        if (tableDefinition.Columns[i].Name != dataTable.Columns[i].ColumnName)
+                            throw new CustomException(400, $"El nombre de la columna '{dataTable.Columns[i].ColumnName}' no coincide con el de la tabla: '{tableDefinition.Columns[i].Name}'.");
+                    }
+
+                    await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
+                }
+                else
+                {
+                    await _importacionRepositories.CreateTable(dataTable, tableName, registryName);
+                    await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(500, ex.Message);
+            }
+        }
+
     }
 }
--- Services/Interface/IImportacionService.cs
+++ Services/Interface/IImportacionService.cs
@@ -3,5 +3,7 @@
     public interface IImportacionService
     {
         Task ImportFromExcel(IFormFile dataset, string registryName, string tableName);
+
+        Task ImportFromTxt(IFormFile dataset, string registryName, string tableName);
     }
 }
