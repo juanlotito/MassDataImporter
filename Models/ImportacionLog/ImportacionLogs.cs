3881995a677fc50f1f21aa9b78c8151631a8bdf4
Juan Ignacio Lotito <jlotito@nosis.com>
2024-05-14 14:27:08 -0300

    feature: add logs model class

---
+++ Models/ImportacionLog/ImportacionLogs.cs
@@ -0,0 +1,19 @@
+﻿namespace importacionmasiva.api.net.Models.ImportacionLog
+{
+    public class ImportacionLog
+    {
+        public int IdImportacionLogs { get; set; }
+        public string TablaDestino { get; set; }
+        public string AccionEliminar { get; set; }
+        public bool CreoTabla { get; set; }
+        public int RegistrosAfectados { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
+        public string TipoArchivo { get; set; }
+        public string NombreArchivo { get; set; }
+        public string DBNombre { get; set; }
+        public string Estado { get; set; }
+        public bool Error { get; set; }
+        public string? DetalleError { get; set; }
+    }
+}
