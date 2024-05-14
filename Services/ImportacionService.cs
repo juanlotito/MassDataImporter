91ea117c8f94014fa8ed8dd56885071c6f9ee3f8
Juan Ignacio Lotito <jlotito@nosis.com>
2024-05-14 14:28:42 -0300

    refactor: changed all methods logic, added logging and use of callbacks avoiding exceptions on extraction

--- Services/ImportacionService.cs
+++ Services/ImportacionService.cs
@@ -12,20 +12,23 @@ namespace importacionmasiva.api.net.Services
 {
     public class ImportacionService : IImportacionService
     {
-        private readonly IImportacionRepositories _importacionRepositories;
+        private readonly IImportacionRepository _importacionRepositories;
+        private readonly IImportacionLogsService _importacionLogsService;
         private readonly IExcelUtils _excelUtils;
         private readonly ICsvUtils _csvUtils;
         private readonly ITxtUtils _txtUtils;
         private readonly TableValidator _tableValidator;
 
         public ImportacionService(
-            IImportacionRepositories importacionRepositories,
+            IImportacionRepository importacionRepositories,
+            IImportacionLogsService importacionLogsService,
             IExcelUtils excelUtils,
             ICsvUtils csvUtils,
             ITxtUtils txtUtils,
             TableValidator tableValidator)
         {
             _importacionRepositories = importacionRepositories;
+            _importacionLogsService = importacionLogsService;
             _excelUtils = excelUtils;
             _csvUtils = csvUtils;
             _txtUtils = txtUtils;
@@ -34,29 +37,39 @@ namespace importacionmasiva.api.net.Services
 
         public async Task ImportFromExcel(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction = DeleteAction.None)
         {
-            DataTable dataTable = _excelUtils.ReadExcelToDataTable(dataset);
-            await ImportData(dataTable, registryName, tableName, deleteAction);
+            //Se pasa como callback la función correspondiente para leer el archivo
+            await ImportData(dataset, registryName, tableName, deleteAction, "xlsx", (file) => _excelUtils.ReadExcelToDataTable(file));
         }
 
         public async Task ImportFromTxt(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction = DeleteAction.None)
         {
-            DataTable dataTable = _txtUtils.ReadTxtToDataTable(dataset);
-            await ImportData(dataTable, registryName, tableName, deleteAction);
+            //Se pasa como callback la función correspondiente para leer el archivo
+            await ImportData(dataset, registryName, tableName, deleteAction, "txt", (file) => _txtUtils.ReadTxtToDataTable(file));
         }
 
         public async Task ImportFromCsv(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction = DeleteAction.None)
         {
-            DataTable dataTable = _csvUtils.ReadCsvToDataTable(dataset);
-            await ImportData(dataTable, registryName, tableName, deleteAction);
+            //Se pasa como callback la función correspondiente para leer el archivo
+            await ImportData(dataset, registryName, tableName, deleteAction, "csv", (file) => _csvUtils.ReadCsvToDataTable(file));
         }
 
-        private async Task ImportData(DataTable dataTable, string registryName, string tableName, DeleteAction deleteAction)
+        private async Task ImportData(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction, string tipoArchivo, Func<IFormFile, DataTable> readFileToDataTable)
         {
-            if (dataTable.Rows.Count == 0)
-                throw new CustomException(400, "El archivo no contiene registros.");
+            if (dataset == null)
+                throw new CustomException(400, "No se ha seleccionado ningún archivo.");
+
+            int logId = -1;
 
             try
             {
+                logId = await _importacionLogsService.LogInicio(tableName, tipoArchivo, dataset.FileName, registryName, deleteAction.ToString(), await _importacionRepositories.TableExists(tableName, registryName));
+
+                //Se ejecuta la función pasada a modo de callback
+                DataTable dataTable = readFileToDataTable(dataset);
+
+                if (dataTable.Rows.Count == 0)
+                    throw new CustomException(400, "El archivo no contiene registros.");
+
                 if (await _importacionRepositories.TableExists(tableName, registryName))
                 {
                     await HandleExistingTable(tableName, registryName, deleteAction, dataTable);
@@ -66,9 +79,16 @@ namespace importacionmasiva.api.net.Services
                     await _importacionRepositories.CreateTable(dataTable, tableName, registryName);
                     await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
                 }
+
+                await _importacionLogsService.LogFinalizacion(logId, false, "Exitoso", registryName, dataTable.Rows.Count);
             }
             catch (Exception ex)
             {
+                if (logId != -1)
+                {
+                    await _importacionLogsService.LogFinalizacion(logId, true, "Anulado", registryName, 0, ex.Message);
+                }
+
                 throw new CustomException(500, ex.Message);
             }
         }
@@ -86,6 +106,6 @@ namespace importacionmasiva.api.net.Services
 
             await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
         }
-    }
 
+    }
 }
