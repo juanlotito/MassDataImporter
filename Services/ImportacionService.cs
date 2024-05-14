732bef493c52fe28a269d6e5fc242d1cef4e8650
Juan Ignacio Lotito <jlotito@nosis.com>
2024-05-14 09:02:28 -0300

    refactor: total refactor on service, modularization changes, added validations and add comun method

--- Services/ImportacionService.cs
+++ Services/ImportacionService.cs
@@ -1,6 +1,8 @@
-﻿using importacionmasiva.api.net.Models.Enum;
+﻿using importacionmasiva.api.net.Helpers;
+using importacionmasiva.api.net.Models.Enum;
 using importacionmasiva.api.net.Repositories.Interface;
 using importacionmasiva.api.net.Services.Interface;
+using importacionmasiva.api.net.Utils.Csv;
 using importacionmasiva.api.net.Utils.Excel;
 using importacionmasiva.api.net.Utils.Exceptions;
 using importacionmasiva.api.net.Utils.Txt;
@@ -11,24 +13,36 @@ namespace importacionmasiva.api.net.Services
     public class ImportacionService : IImportacionService
     {
         private readonly IImportacionRepositories _importacionRepositories;
-        private readonly ExcelUtils _excelUtils = new ExcelUtils();
-        private readonly TxtUtils _txtUtils = new TxtUtils();
+        private readonly IExcelUtils _excelUtils;
+        private readonly ICsvUtils _csvUtils;
+        private readonly ITxtUtils _txtUtils;
+        private readonly TableValidator _tableValidator;
 
-        public ImportacionService(IImportacionRepositories importacionRepositories)
+        public ImportacionService(
+            IImportacionRepositories importacionRepositories,
+            IExcelUtils excelUtils,
+            ICsvUtils csvUtils,
+            ITxtUtils txtUtils,
+            TableValidator tableValidator)
         {
             _importacionRepositories = importacionRepositories;
+            _excelUtils = excelUtils;
+            _csvUtils = csvUtils;
+            _txtUtils = txtUtils;
+            _tableValidator = tableValidator;
         }
 
         public async Task ImportFromExcel(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction = DeleteAction.None)
         {
-            try
-            {
-                DataTable dataTable = _excelUtils.ReadExcelToDataTable(dataset);
+            DataTable dataTable = _excelUtils.ReadExcelToDataTable(dataset);
+            await ImportData(dataTable, registryName, tableName, deleteAction);
+        }
 
-                if (await _importacionRepositories.TableExists(tableName, registryName))
-                {
-                    if (deleteAction == DeleteAction.Delete)
-                        await _importacionRepositories.DeleteRecords(tableName, registryName);
+        public async Task ImportFromTxt(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction = DeleteAction.None)
+        {
+            DataTable dataTable = _txtUtils.ReadTxtToDataTable(dataset);
+            await ImportData(dataTable, registryName, tableName, deleteAction);
+        }
 
         public async Task ImportFromCsv(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction = DeleteAction.None)
         {
@@ -36,56 +50,16 @@ namespace importacionmasiva.api.net.Services
             await ImportData(dataTable, registryName, tableName, deleteAction);
         }
 
-                    var tableDefinition = await _importacionRepositories.GetTableDefinition(tableName, registryName);
-
-                    if (tableDefinition.Columns.Count != dataTable.Columns.Count)
-                        throw new CustomException(400, "El número de columnas no coincide con el de la tabla existente.");
-
-                    for (int i = 0; i < tableDefinition.Columns.Count; i++)
-                    {
-                        if (tableDefinition.Columns[i].Name != dataTable.Columns[i].ColumnName)
-                            throw new CustomException(400, $"El nombre de la columna '{dataTable.Columns[i].ColumnName}' no coincide con el de la tabla: '{tableDefinition.Columns[i].Name}'.");
-                    }
-
-                    await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
-                }
-                else
-                {
-                    await _importacionRepositories.CreateTable(dataTable, tableName, registryName);
-                    await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new CustomException(500, ex.Message);
-            }
-        }
-
-        public async Task ImportFromTxt(IFormFile dataset, string registryName, string tableName, DeleteAction deleteAction = DeleteAction.None)
+        private async Task ImportData(DataTable dataTable, string registryName, string tableName, DeleteAction deleteAction)
         {
+            if (dataTable.Rows.Count == 0)
+                throw new CustomException(400, "El archivo no contiene registros.");
+
             try
             {
-                DataTable dataTable = _txtUtils.ReadTxtToDataTable(dataset);
-
                 if (await _importacionRepositories.TableExists(tableName, registryName))
                 {
-                    if (deleteAction == DeleteAction.Delete)
-                        await _importacionRepositories.DeleteRecords(tableName, registryName);
-                    else if (deleteAction == DeleteAction.Truncate)
-                        await _importacionRepositories.TruncateTable(tableName, registryName);
-
-                    var tableDefinition = await _importacionRepositories.GetTableDefinition(tableName, registryName);
-
-                    if (tableDefinition.Columns.Count != dataTable.Columns.Count)
-                        throw new CustomException(400, "El número de columnas no coincide con el de la tabla existente.");
-
-                    for (int i = 0; i < tableDefinition.Columns.Count; i++)
-                    {
-                        if (tableDefinition.Columns[i].Name != dataTable.Columns[i].ColumnName)
-                            throw new CustomException(400, $"El nombre de la columna '{dataTable.Columns[i].ColumnName}' no coincide con el de la tabla: '{tableDefinition.Columns[i].Name}'.");
-                    }
-
-                    await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
+                    await HandleExistingTable(tableName, registryName, deleteAction, dataTable);
                 }
                 else
                 {
@@ -99,5 +73,19 @@ namespace importacionmasiva.api.net.Services
             }
         }
 
+        private async Task HandleExistingTable(string tableName, string registryName, DeleteAction deleteAction, DataTable dataTable)
+        {
+            if (deleteAction == DeleteAction.Delete)
+                await _importacionRepositories.DeleteRecords(tableName, registryName);
+            else if (deleteAction == DeleteAction.Truncate)
+                await _importacionRepositories.TruncateTable(tableName, registryName);
+
+            var tableDefinition = await _importacionRepositories.GetTableDefinition(tableName, registryName);
+
+            _tableValidator.ValidateTableDefinition(tableDefinition, dataTable);
+
+            await _importacionRepositories.BulkInsert(dataTable, tableName, registryName);
+        }
     }
+
 }
